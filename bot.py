20eae00b8e1163e07e5ee921b2e5df104da59c0b
SuperPlayArt <67067612+SuperPlayArt@users.noreply.github.com>
2021-04-11 16:12:46 +0200

    Ajout de plusieurs commandes !

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ client.remove_command("help")
 @client.event
 async def on_ready():
     print('Bot is ready')
-    
+    bot. load_extension('cogs.leveling')
 
 
 @client.command(aliases=['invite'])
@@ -21,8 +21,19 @@ async def alertelog(ctx):
     await ctx.send('https://media.discordapp.net/attachments/797593407513559080/830180368853893180/exit.png?width=1440&height=529')
 
 
+@client.command(aliases=['spbinvite'])
+async def invitebot(ctx):
+    embed=discord.Embed(title="Clique ici pour invité le bot !", url="https://discord.com/api/oauth2/authorize?client_id=820733417640951808&permissions=2113793271&scope=bot", description="Ce bot est encore en bêta, donc ne lui en demander pas trop : )", color=0x9900ff)
+    embed.set_thumbnail(url="https://media.discordapp.net/attachments/797593407513559080/830098585772884028/Logo_SPA_v2.png?width=1240&height=676")
+    embed.set_footer(text="2021 - SPA")
+    await ctx.send(embed=embed)
 
-
+@client.command(aliases=['don'])
+async def spa(ctx):
+    embed=discord.Embed(title="Cliquez ici pour faire un don à la SPA ", url="https://soutenir.la-spa.fr/b/mon-don", description="(la vrai, pas à SuperPlayArt)", color=0x9900ff)
+    embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/fr/thumb/0/00/Logo_de_la_SPA_%28France%29.png/800px-Logo_de_la_SPA_%28France%29.png")
+    embed.set_footer(text="2021 - SPA")
+    await ctx.message.reply(embed=embed)
 
 @client.command()
 async def info(ctx):
@@ -32,8 +43,10 @@ async def info(ctx):
     embed.add_field(name="Créateur : ", value="SPA (SuperPlayArt)", inline=True)
     embed.add_field(name="Langages :", value="Python", inline=True)
     embed.add_field(name="Préfix :", value="!s", inline=True)
+    embed.add_field(name="GitHub :", value="[Cliquez ici](https://github.com/SuperPlayArt/SuperPlayBot)", inline=True)
     embed.set_footer(text="2021 - SPA")
     await ctx.message.reply(embed=embed)
+
 
 @client.command(aliases=['pp'])
 async def avatar(ctx, *, member: discord.Member=None): # set the member object to None
@@ -50,10 +63,15 @@ async def help(ctx):
     embed=discord.Embed(title="SuperPlayBot - Les commandes", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ", description="Voici les commandes du bot !", color=0x9900ff)
     embed.set_thumbnail(url="https://media.discordapp.net/attachments/797593407513559080/830098585772884028/Logo_SPA_v2.png?width=1240&height=676")
     embed.add_field(name="!s help ", value="Actuellement utilisé.", inline=False)
+    embed.add_field(name="!s info ", value="Permet d\'avoir les infos sur le bot.", inline=False)
+    embed.add_field(name="!s invitebot ", value="Permet d\'invité le bot sur votre serveur.", inline=False)
+    embed.add_field(name="!s invite ", value="Permet d'avoir le lien du serveur.", inline=False)
     embed.add_field(name="!s avatar", value="Pour avoir ton avatar ou celui des autres.", inline=True)
     embed.add_field(name="!s serv", value="Pioche au hasard un serveur dans une liste.", inline=True)
+    embed.add_field(name="!s userinfo", value="Permet d\'avoir les information d'un utilisateur.", inline=True)
     embed.add_field(name="!s alertelog", value="Quand une blague nulle est faites utilisé cette commande.", inline=True)
     embed.add_field(name="!s say", value="Parler en tant que SPB. (Ne pas en abuser, sinon c'est lourd)", inline=True)
+    embed.add_field(name="!s spa", value="Aidez la SPA.", inline=True)
     embed.set_footer(text="2021 - SPA")
     await ctx.message.reply(embed=embed)
 
@@ -76,28 +94,28 @@ async def on_ready():
 
 @client.command(aliases=['dire'])
 async def say(ctx, *texte, member: discord.Member=None):
-    member = ctx.message.author
+    member = ctx.message.author.name
     embed=discord.Embed(title=" ".join(texte), color=0x9900ff)
-    embed.set_footer(text=member)
+    embed.set_footer(text=f'{member} à utilisé !s say')
     await ctx.message.delete()
     await ctx.send(embed=embed)
 
-
 @client.command()
-async def vaporwave(ctx, *text):
-    VaporChar = "ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ"
-    VaporText = []
-    for word in text:
-        for char in word:
-            if char.isalpha():
-                index = ord(char) - ord("a")
-                transformed = VaporChar [index]
-                VaporText.append(transformed)
-            else:
-                VaporText.append(char)
-            VaporText.append(" ")
-    await ctx.send("".join(VaporText))
+async def userinfo(ctx, *, member: discord.Member=None):
+    if not member:
+        member = ctx.message.author
+    username = member.name
+    userAvatar = member.avatar_url
+    usercreation = member.created_at.strftime("%A, %B %d %Y at %H:%M:%S %p")
+    userjoin = member.joined_at.strftime("%A, %B %d %Y at %H:%M:%S %p")
+    embed=discord.Embed(title=f'**Voici les infos de {username} !**', color=0x9900ff)
+    embed.set_thumbnail(url=member.avatar_url)
+    embed.add_field(name="Nom d'utilisateur :", value=member, inline=False)
+    embed.add_field(name="Date de création :", value=usercreation, inline=False)
+    embed.add_field(name="Date d'arrivée :", value=userjoin, inline=False)
+    embed.set_footer(text="2021 - SPA", icon_url="https://media.discordapp.net/attachments/797593407513559080/830098585772884028/Logo_SPA_v2.png?width=1240&height=676")
+    await ctx.message.reply(embed=embed)
 
 
 
-client.run('TOKEN HERE')
+client.run('TOKEN')
